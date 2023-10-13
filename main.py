3dc57484cb470551cd03f740c78fa146c277e4e8
Tech5818 <pqow5818@gmail.com>
2023-10-13 18:01:33 +0900

    네이버 웹툰 랭킹 json 저장

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import json
 
 # 웹 드라이버 설정
 s = Service("chromedriver.exe")
@@ -13,7 +14,16 @@ driver.get(url)
 driver.implicitly_wait(10)
 elements = driver.find_elements(by=By.CSS_SELECTOR, value="#container > div.content_wrap > div.Aside__aside_wrap--iF5ju.Aside__type_home_logout--R6qSd > div:nth-child(2) > ul > li > div.AsideList__info_area--PVPwn > a.ContentTitle__title_area--x24vt > span > span")
 
+i = 1
+rank ={}
 for element in elements:
-    print(element.text)
+    rank[i] = element.text
+    i = i+1
+print(rank)
+
+json_data = json.dumps(rank, ensure_ascii=False, indent=2)
+
+with open("data.json", "w", encoding="utf-8") as json_file:
+    json_file.write(json_data)
 
 driver.quit()
